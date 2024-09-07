c8f4af813c6b83a2e10b920decb51c4e5d7a3c99
Chris <atnl@protonmail.com>
2024-09-07 09:27:25 -0400

    Update theme.lua

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,14 +1,16 @@
 return
-  {
+  { 
     -- Rose Pine theme
-    'rose-pine/neovim',
+    -- 'rose-pine/neovim', Uncomment to use Rose Pine
+    -- Tokyo Night
+    'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'tokyonight-night'
+    -- vim.cmd.colorscheme 'rose-pine' -- Uncomment to activate rose-pine
       require('mason-lspconfig').setup()
 
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   }
-
