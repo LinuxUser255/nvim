5e27946f0de54751bda878944a4822c7112862f7
Chris <atnl@protonmail.com>
2024-12-15 02:19:09 -0500

    Update colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,5 @@
 return
-  { 
+  {
     -- Rose Pine theme
     -- 'rose-pine/neovim', Uncomment to use Rose Pine
     -- Tokyo Night
@@ -10,7 +10,13 @@ return
     -- vim.cmd.colorscheme 'rose-pine' -- Uncomment to activate rose-pine
       require('mason-lspconfig').setup()
 
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.api.nvim_set_hl(0, "Normal", { bg = "#1e1e2e"}) -- Set background color to a dark gray
+            -- vim.api.nvim_set_hl(0, "Normal", { bg = "#2E3436"}) -- Set background color to a dark gray
+            --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2E3436"}) -- Set background color for normal
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e"}) -- Set background color for normal
+-- float
+
+           -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+           -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   }
