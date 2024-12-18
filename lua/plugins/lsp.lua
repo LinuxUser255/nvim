9293fecf948926211be6629b51495e01f0869125
Chris <atnl@protonmail.com>
2024-12-18 09:10:38 -0500

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,8 +27,12 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                -- "rust_analyzer",
-                -- "gopls",
+                 "rust_analyzer",
+                --"gopls", -- need to properly install go
+                 "pyright",
+                 "clanged",
+                 "bashls",
+                 "ts-ls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
