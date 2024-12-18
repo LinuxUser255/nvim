b741b8bbb625b7119f243908c7c4da3d37f3fddf
Chris <atnl@protonmail.com>
2024-12-18 09:07:54 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,3 +2,11 @@
 -- Neovim runtime path, (rtp) looks for this entrypoint: init.lua
 -- located: $XDG_CONFIG_HOME/nvim/init.lua
 require('config')
+require('tabnine').setup({
+  disable_auto_comment=true,
+  accept_keymap="<Tab>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = {gui = "#808080", cterm = 244},
+  exclude_filetypes = {"TelescopePrompt"}
+})
