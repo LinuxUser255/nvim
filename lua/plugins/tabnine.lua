349f3706c79bb7df481da599445be6efc7d5b054
LinuxUser255 <atnl@protonmailcom>
2025-06-12 09:48:26 -0400

    activated tabnine and added more keymaps to the tabnine.lua plugin

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -18,6 +18,18 @@ return {
     vim.keymap.set("n", "<leader>tl", "<cmd>TabnineLogin<CR>", { desc = "Tabnine Login" })
     vim.keymap.set("n", "<leader>ts", "<cmd>TabnineStatus<CR>", { desc = "Tabnine Status" })
     vim.keymap.set("n", "<leader>tt", "<cmd>TabnineToggle<CR>", { desc = "Toggle Tabnine" })
+    vim.keymap.set("n", "<leader>tn", "<cmd>TabnineNext<CR>", { desc = "Next Tabnine suggestion" })
+    -- When user highligts code tabnine provides option to edit the code with Tabnine
+    -- Edit with tabnine
+    vim.keymap.set("n", "<leader>tw", "<cmd>TabnineEdit<CR>", { desc = "Edit with Tabnine" })
+    -- Fix this code
+    vim.keymap.set("n", "<leader>tf", "<cmd>TabnineFix<CR>", { desc = "Fix code with Tabnine" })
+    -- Explain this code
+    vim.keymap.set("n", "<leader>te", "<cmd>TabnineExplain<CR>", { desc = "Explain code with Tabnine" })
+    -- Document this code
+    vim.keymap.set("n", "<leader>td", "<cmd>TabnineDocument<CR>", { desc = "Document code with Tabnine" })
+    -- Generate test
+    vim.keymap.set("n", "<leader>tg", "<cmd>TabnineGenerate<CR>", { desc = "Generate test with Tabnine" })
 
     -- Single debugging command for future troubleshooting
     vim.api.nvim_create_user_command('TabnineDebug', function()
