941a650fc1059ce00d1ec5924b7784fd76134566
LinuxUser255 <atnl@protonmailcom>
2025-06-29 15:47:00 -0400

    activated harpoon & created a harpoon usermanual

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,4 +1,5 @@
-return {
+return
+{
     "ThePrimeagen/harpoon",
 
     branch = "harpoon2",
@@ -7,5 +8,33 @@ return {
     },
 
     config = function()
+        local harpoon = require('harpoon')
+
+        -- Define the toggle_telescope function
+        local function toggle_telescope(harpoon_list)
+            local telescope = require("telescope.builtin")
+            local conf = require("telescope.config").values
+            local file_paths = {}
+            for _, item in ipairs(harpoon_list.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            telescope.find_files({
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            })
+        end
+
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>fl", function() toggle_telescope(harpoon:list()) end,
+            { desc = "Open harpoon window" })
+            -- Change the keympa below at some point.
+            vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
     end
 }
