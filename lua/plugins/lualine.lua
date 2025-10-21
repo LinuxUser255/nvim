dbd6ba3a0b9b816e3f422deddae511b91588ddd1
LinuxUser255 <atnl@protonmail.com>
2025-10-21 16:20:13 -0400

    fix: patch vim-be-good plugin and update lualine config…

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,8 @@ return {
   config = function()
     require('lualine').setup({
       options = {
-        theme = 'auto',
+        theme = 'horizon',
+        icons_enabled = true,
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
