e5a1603a31f6da7c0fcbf26e70faef14f3b5fa53
Chris Bingham <chris@Mac.lan>
2025-11-11 14:35:22 -0500

    macOS: Disable typescript-tools.nvim due to Neovim 0.11 compatibility
    - Plugin has validation issues with Neovim 0.11-dev
    - Keeping config simple and stable for macOS branch
    - Other TypeScript plugins still available for TS support

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,60 +1,10 @@
 return {
     -- TypeScript/JavaScript specific plugins
-    {
-        "pmizio/typescript-tools.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "neovim/nvim-lspconfig",
-        },
-        -- Remove the ft restriction and use event instead
-        event = { "BufReadPre", "BufNewFile" },
-        -- Keep the filetypes for conditional setup
-        filetypes = {
-            "typescript",
-            "javascript",
-            "typescriptreact",
-            "javascriptreact",
-            "typescript.tsx",
-            "javascript.jsx",
-        },
-        opts = {
-            settings = {
-                -- spawn additional tsserver instance to calculate diagnostics on it
-                separate_diagnostic_server = true,
-                -- "change"|"insert_leave" determine when the client asks the server about diagnostic
-                publish_diagnostic_on = "insert_leave",
-                -- expose all actions as code actions
-                expose_as_code_action = "all",
-                -- enhanced completion experience
-                complete_function_calls = true,
-                include_completions_with_insert_text = true,
-                -- CodeLens
-                code_lens = "all",
-                -- Auto-import settings
-                auto_import_suggestions = {
-                    enable = true,
-                },
-            },
-        },
-        config = function(_, opts)
-            require("typescript-tools").setup(opts)
-
-            -- Adding TypeScript specific keymaps - modify to check filetype
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = {"typescript", "javascript", "typescriptreact", "javascriptreact"},
-                callback = function()
-                    local buffer = vim.api.nvim_get_current_buf()
-                    vim.keymap.set("n", "<leader>toi", ":TSToolsOrganizeImports<CR>", { buffer = buffer })
-                    vim.keymap.set("n", "<leader>tru", ":TSToolsRemoveUnused<CR>", { buffer = buffer })
-                    vim.keymap.set("n", "<leader>tfi", ":TSToolsFixAll<CR>", { buffer = buffer })
-                    vim.keymap.set("n", "<leader>tai", ":TSToolsAddMissingImports<CR>", { buffer = buffer })
-                    vim.keymap.set("n", "<leader>tsu", ":TSToolsSortImports<CR>", { buffer = buffer })
-                    vim.keymap.set("n", "<leader>trf", ":TSToolsRenameFile<CR>", { buffer = buffer })
-                    vim.keymap.set("n", "<leader>tgd", ":TSToolsGoToSourceDefinition<CR>", { buffer = buffer })
-                end,
-            })
-        end,
-    },
+    -- macOS branch: typescript-tools.nvim disabled due to Neovim 0.11 compatibility issues
+    -- {
+    --     "pmizio/typescript-tools.nvim",
+    --     enabled = false,
+    -- },
     {
         "dmmulroy/tsc.nvim",
         cmd = { "TSC" },
